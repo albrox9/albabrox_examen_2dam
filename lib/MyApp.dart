36d3d54ba09fb842f3118d77aa86f14a6f5f4d1b
albrox9 <damalbrox9@gmail.com>
2022-11-24 19:50:11 +0100

    Añado las rutas Creo la SplashView

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -9,14 +9,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
-  }
+      initialRoute: '/splashview',
+      routes: {
+        '/splashview': (context) => SplashView(),
+        '/details': (context) => DetailScreen(),
+      };
+    }
 }
